61c6978fe0c506c13fb8a8830c4267a8812689ee
danishpathan251 <eng.arkee@gmail.com>
2025-08-07 23:09:31 +0530

    commit the chanhges

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -117,9 +117,10 @@ console.log(data);
                     : "bg-gray-700 text-white rounded-tr-md"
                 }`}
               >
-                <p className="text-sm leading-relaxed whitespace-pre-line">
-                  dangerouslySetInnerHTML={{ __html: msg.text }}
-                </p>
+          <div
+  className="text-sm leading-relaxed whitespace-pre-line"
+  dangerouslySetInnerHTML={{ __html: msg.text }}
+/>
               </div>
               <div
                 className={`text-xs text-gray-500 mt-1 px-2 ${
